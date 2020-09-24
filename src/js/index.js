a0273f84a21b38ef8698310d90149684b719f590
william <willpickeral@gmail.com>
2020-09-23 22:11:47 -0400

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,3 @@
 import num from './test';
 console.log("I imported " + num + " from another module called test.js!");
+console.log('Hello, world!');
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,6 @@
+import { log } from 'console';
 import num from './test';
 
 console.log(`I imported ${num} from another module called test.js!`);
+console.log('Hello, world!');
+
